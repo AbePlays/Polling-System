544d7a856555be2afcfe113235a75dfe2281c472
Abe <abhi.rawat456@gmail.com>
2020-09-08 10:59:48 +0530

    Testing cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,12 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// https request 1
+exports.randomNumber = functions.https.onRequest((req, res) => {
+  const number: Number = Math.round(Math.random() * 100);
+  res.send(number.toString());
+});
+
+// https request 2
+exports.lesgo = functions.https.onRequest((req, res) => {
+  res.redirect("https://abeplays.github.io");
+});
