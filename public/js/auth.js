27e11c9e2383225b9d133c159c58d0b54038d4eb
Abe <abhi.rawat456@gmail.com>
2020-09-08 11:44:38 +0530

    Registering user to firebase

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,9 +1,29 @@
 var authSwitchLinks = document.querySelectorAll(".switch");
 var authModals = document.querySelectorAll(".auth .modal");
 var authWrapper = document.querySelector(".auth");
+var registerForm = document.querySelector(".register");
+var loginForm = document.querySelector(".login");
+var signOut = document.querySelector(".sign-out");
 // toggle auth modals
 authSwitchLinks.forEach(function (link) {
     link.addEventListener("click", function () {
         authModals.forEach(function (modal) { return modal.classList.toggle("active"); });
+    });
+});
+// register user
+registerForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var email = registerForm.email.value;
+    var password = registerForm.password.value;
+    // @ts-ignore
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(function (user) {
+        console.log("User registered", user);
+        registerForm.reset();
+    })["catch"](function (e) {
+        console.log(e);
+        registerForm.querySelector(".error").textContent = e.message;
     });
 });
--- public/js/auth.ts
+++ public/js/auth.ts
@@ -1,10 +1,32 @@
 const authSwitchLinks = document.querySelectorAll(".switch");
 const authModals = document.querySelectorAll(".auth .modal");
 const authWrapper = document.querySelector(".auth");
+const registerForm = document.querySelector(".register") as HTMLFormElement;
+const loginForm = document.querySelector(".login") as HTMLFormElement;
+const signOut = document.querySelector(".sign-out");
 
 // toggle auth modals
 authSwitchLinks.forEach((link) => {
   link.addEventListener("click", () => {
     authModals.forEach((modal) => modal.classList.toggle("active"));
   });
+});
+
+// register user
+registerForm.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  const email = registerForm.email.value;
+  const password = registerForm.password.value;
+  // @ts-ignore
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((user) => {
+      console.log("User registered", user);
+      registerForm.reset();
+    })
+    .catch((e) => {
+      registerForm.querySelector(".error").textContent = e.message;
+    });
 });
