c416fb491f9dfe4ae82d404b981a06a6bcc6cf25
Abe <abhi.rawat456@gmail.com>
2020-09-08 12:35:18 +0530

    Created cloud function to add new polls

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,3 +17,24 @@ exports.userDeleted = functions.auth.user().onDelete((user) => {
   const doc = admin.firestore().collection("user").doc(user.uid);
   return doc.delete();
 });
+
+// http to add a poll
+exports.addPoll = functions.https.onCall((data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      "unauthenticated",
+      "Only authenticated users can add polls"
+    );
+  }
+  if (data.text.length > 30) {
+    throw new functions.https.HttpsError(
+      "invalid-argument",
+      "Request must be no more than 30 characters long"
+    );
+  }
+
+  return admin.firestore().collection("requests").add({
+    text: data.text,
+    upvotes: 0,
+  });
+});
